f1496f25e63b838a7fa2333ee350c42c11b7828a
goga <goga@GOGAL-LIN>
2023-05-11 13:56:34 +0300

    SkIDE core now uses fragment+Addon instead of Quant

---
+++ org.toxsoft.skide.core/src/org/toxsoft/skide/core/e4/addons/AddonSkideCore.java
@@ -0,0 +1,49 @@
+package org.toxsoft.skide.core.e4.addons;
+
+import org.eclipse.e4.core.contexts.*;
+import org.toxsoft.core.tsgui.bricks.quant.*;
+import org.toxsoft.core.tsgui.mws.bases.*;
+import org.toxsoft.skf.users.gui.*;
+import org.toxsoft.skide.core.Activator;
+import org.toxsoft.skide.core.api.impl.*;
+import org.toxsoft.uskat.core.gui.*;
+
+/**
+ * The plugin addon.
+ *
+ * @author hazard157
+ */
+public class AddonSkideCore
+    extends MwsAbstractAddon {
+
+  /**
+   * Constructor.
+   */
+  public AddonSkideCore() {
+    super( Activator.PLUGIN_ID );
+  }
+
+  // ------------------------------------------------------------------------------------
+  // MwsAbstractAddon
+  //
+
+  @Override
+  protected void doRegisterQuants( IQuantRegistrator aQuantRegistrator ) {
+    aQuantRegistrator.registerQuant( new QuantSkCoreGui() );
+    aQuantRegistrator.registerQuant( new QuantSkUsersGui() );
+    aQuantRegistrator.registerQuant( new QuantSkide010Workroom() );
+    aQuantRegistrator.registerQuant( new QuantSkide020SkConnection() );
+    aQuantRegistrator.registerQuant( new QuantSkide030Environment() );
+  }
+
+  @Override
+  protected void initApp( IEclipseContext aAppContext ) {
+    // nop
+  }
+
+  @Override
+  protected void initWin( IEclipseContext aWinContext ) {
+    // nop
+  }
+
+}
