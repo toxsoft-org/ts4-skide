26475ae920cfd05761af2be7b061de0b001d3369
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2023-09-05 12:32:21 +0300

    первая версия редактора (скорее просмотрщика) объектов

--- org.toxsoft.skide.plugin.sded/src/org/toxsoft/skide/plugin/sded/main/SkideUnitPanelObjects.java
+++ org.toxsoft.skide.plugin.sded/src/org/toxsoft/skide/plugin/sded/main/SkideUnitPanelObjects.java
@@ -1,11 +1,10 @@
 package org.toxsoft.skide.plugin.sded.main;
 
-import org.eclipse.swt.*;
-import org.eclipse.swt.custom.*;
 import org.eclipse.swt.widgets.*;
 import org.toxsoft.core.tsgui.bricks.ctx.*;
 import org.toxsoft.skide.core.api.*;
 import org.toxsoft.skide.core.api.impl.*;
+import org.toxsoft.uskat.core.gui.glib.*;
 
 /**
  * {@link AbstractSkideUnitPanel} implementation.
@@ -21,9 +20,8 @@ class SkideUnitPanelObjects
 
   @Override
   protected Control doCreateControl( Composite aParent ) {
-    CLabel label = new CLabel( aParent, SWT.CENTER );
-    label.setText( "ახტუნგ! ახტუნგ!\nУндер девелопмент!" );
-    return label;
+    SdedObjectEditor sdedObjects = new SdedObjectEditor( tsContext(), null );
+    return sdedObjects.createControl( aParent );
   }
 
 }
