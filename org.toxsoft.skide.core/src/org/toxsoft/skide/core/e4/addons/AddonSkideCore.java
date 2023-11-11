7943edcdcb8632b537a222f3640d4fddeba1dca9
goga <goga@GOGAL-LIN>
2023-11-11 11:16:07 +0300

    QuantSkide022SkConnection - work with SQLite built-in DB instead of TXT

--- org.toxsoft.skide.core/src/org/toxsoft/skide/core/e4/addons/AddonSkideCore.java
+++ org.toxsoft.skide.core/src/org/toxsoft/skide/core/e4/addons/AddonSkideCore.java
@@ -32,7 +32,12 @@ public class AddonSkideCore
     aQuantRegistrator.registerQuant( new QuantSkCoreGui() );
     aQuantRegistrator.registerQuant( new QuantSkUsersGui() );
     aQuantRegistrator.registerQuant( new QuantSkide010Workroom() );
-    aQuantRegistrator.registerQuant( new QuantSkide020SkConnection() );
+
+    // textual or SQLite?
+    // aQuantRegistrator.registerQuant( new QuantSkide020SkConnection() ); // Textual
+    aQuantRegistrator.registerQuant( new QuantSkide022SkConnection() ); // SQLite
+    // ---
+
     aQuantRegistrator.registerQuant( new QuantSkide030Environment() );
   }
 
